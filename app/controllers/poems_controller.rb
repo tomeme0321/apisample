014c64d3131daec664fc3cb8dd2ec0975cd3df0d
Keisuke Saotome <Keisuke@keisukes-MacBook-Air.local>
2016-12-25 17:33:21 +0900

    Dive14-2 on going

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -3,4 +3,13 @@ class PoemsController < ApplicationController
     @poems = Poem.all
     render json: @poems
   end
+  def show
+    @poems = Poem.find(params[:id])
+    render json: @poems
+  end
+
+  private
+  def poemss_params
+    params.require(:poem).permit(:title, :content)
+  end
 end
